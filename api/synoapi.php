cef341a0b8b056398798416aa3f08d8a7ba99ca7
Sammy BAUER <github@sametflo.fr>
2021-11-21 15:33:17 +0100

    Many changes
    rewrite login and logout function to be compatible DSM 7 (using cookies)
    set request path from api info function

--- api/synoapi.php
+++ api/synoapi.php
@@ -9,90 +9,129 @@ class synoapi
   private $password;
   private $url;
   private $curl;
-  private $cookiefile;
   private $response;
-  private $token;
+  private $loggedin;
+  private $services;
 
   function __construct($url, $username, $password)
   {
-    $this->lastError = '';
-    $this->lastErrorCode = 0;
+    $this->setError(0, '');
     $this->username = $username;
     $this->password = $password;
     $this->url = $url;
-    $this->cookiefile = tempnam(sys_get_temp_dir(), 'cookie');
 
     if (!function_exists('curl_init'))
       throw new Exception('php cURL extension must be installed and enabled');
 
     $this->curl = curl_init();
     curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($this->curl, CURLOPT_COOKIEJAR, $this->cookiefile);
+    curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, 0); // must not verify host if not valid yet
+    curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, 0); // must not verify peer (same reason)
+    curl_setopt($this->curl, CURLOPT_COOKIEFILE, ''); // Activate cookie engine
 
-    $this->Login();
+    if(!$this->info()){
+      $this->loggedin = false;
+      $this->internalError('API info not found');
+      return;
+    } else
+      $this->services = $this->response->data;
+
+    $this->loggedin = $this->login();
   }
 
   function __destruct()
   {
-    $this->Logout();
-    unlink($this->cookiefile);
+    $this->logout();
+    curl_close($this->curl);
   }
 
-  private function syno_success()
+  private function setError($code, $msg)
   {
-    $this->lastError = '';
-    $this->lastErrorCode = 0;
-    if ($this->response->success)
-      return true;
-
-    if(isset($this->response->error->code)) {
-      $this->lastError = 'SYNO Error code = ' . $this->response->error->code;
-      $this->lastErrorCode = $this->response->error->code;
-    } else
-      $this->lastError = 'Unknown error' . $this->response;
-
-    return false;
+    $this->lastErrorCode = $code;
+    $this->lastError = $msg;
   }
 
-  public function GetLastError()
+  private function internalError($msg)
+  {
+    $this->setError(-1, $msg);
+  }
+
+  public function getLastError()
   {
     return $this->lastError;
   }
 
-  public function Request($path)
+  public function getLastErrorCode()
   {
-    if($this->token != '')
-      $params = '&SynoToken=' . $this->token;
-    else
-      $params = '';
+    return $this->lastErrorCode;
+  }
 
-    curl_setopt($this->curl, CURLOPT_URL, $this->url . $path . $params);
+  private function success()
+  {
+    $this->setError(0, '');
+
+    if (!$this->response) {
+      $this->internalError('Unknown error : no data');
+      return false;
+    }
+
+    if ($this->response->success)
+      return true;
+
+    if(isset($this->response->error->code)) {
+      $this->setError($this->response->error->code, 'SYNO Error code = ' . $this->response->error->code);
+    } else
+      $this->internalError('Unknown error : invalid data');
+
+    return false;
+  }
+
+  public function connected()
+  {
+    return $this->loggedin;
+  }
+
+  public function directRequest($path)
+  {
+    curl_setopt($this->curl, CURLOPT_URL, $this->url . $path);
     $this->response = json_decode(curl_exec($this->curl));
-    return $this->syno_success();
+    return $this->success();
   }
 
-  public function Login()
+  public function request($service, $version, $method, $params = '')
   {
-    if($result = $this->Request('/webman/login.cgi?username=' . $this->username . '&passwd=' . $this->password . '&enable_syno_token=yes'))
-      $this->token = $this->response->SynoToken;
-    return $result;
+    if(!isset($this->services->$service->path)){
+      $this->internalError('Service does not exist : ' . $service);
+      return false;
+    }
+
+    $path = '/webapi/' . $this->services->$service->path . '?api=' . $service . '&version=' . $version . '&method=' . $method . $params;
+    return $this->directRequest($path);
   }
 
-  public function Logout()
+  public function info()
   {
-    curl_setopt($this->curl, CURLOPT_URL, $this->url . '/webman/logout.cgi');
-    curl_exec($this->curl);
-    $this->token = '';
+    return $this->directRequest('/webapi/query.cgi?api=SYNO.API.Info&version=1&method=query');
   }
 
-  public function dump_response()
+  public function login()
+  {
+    return $this->request('SYNO.API.Auth', 3, 'login', '&account=' . $this->username . '&passwd=' . $this->password);
+  }
+
+  public function logout()
+  {
+    return $this->request('SYNO.API.Auth', 3, 'logout');
+  }
+
+  public function dumpResponse()
   {
     var_dump($this->response);
   }
 
   public function Network_Info($ifname)
   {
-    if($this->Request("/webapi/entry.cgi?api=SYNO.Core.Network.LocalBridge&method=get&version=1&ifname=$ifname"))
+    if($this->request('SYNO.Core.Network.LocalBridge', 1, 'get', "&ifname=$ifname"))
       return $this->response;
     else
       return false;
@@ -100,24 +139,24 @@ class synoapi
 
   public function NSM_Devices()
   {
-    if($this->Request('/webapi/entry.cgi?api=SYNO.Core.Network.NSM.Device&method=get&version=4&conntype="all"'))
+    if($this->request('SYNO.Core.Network.NSM.Device', 4, 'get', '&conntype="all"'))
       return $this->response;
     else
       return false;
   }
 
-  public function SearchCertificates()
+  public function searchCertificates()
   {
-    return $this->Request('/webapi/entry.cgi?api=SYNO.Core.Certificate.CRT&method=list&version=1');
+    return $this->request('SYNO.Core.Certificate.CRT', 1, 'list');
   }
 
-  public function UpdateCertificate($certname, $key, $cert, $chain)
+  public function updateCertificate($certname, $key, $cert, $chain)
   {
     $id = '';
     $desc = '';
     $default = 'false';
 
-    if($this->SearchCertificates()) {
+    if($this->searchCertificates()) {
 
       if(!isset($this->response->data->certificates))
         return false;
@@ -133,64 +172,62 @@ class synoapi
     }
 
     $post = new multipart_data();
-    $post->addfile('key', $key);
-    $post->addfile('cert', $cert);
-    $post->addfile('inter_cert', $chain);
-    $post->addpart('id', $id);
-    $post->addpart('desc', $desc);
-    $post->addpart('as_default', $default);
-    $boundary = $post->get_boundary();
-    $post = $post->get_postdata();
+    $post->addFile('key', $key);
+    $post->addFile('cert', $cert);
+    $post->addFile('inter_cert', $chain);
+    $post->addPart('id', $id);
+    $post->addPart('desc', $desc);
+    $post->addPart('as_default', $default);
+    $data = $post->postdata();
 
-    curl_setopt($this->curl, CURLOPT_HTTPHEADER, array("Content-Type: multipart/form-data; boundary=$boundary","Content-Length: " . strlen($post)));
+    curl_setopt($this->curl, CURLOPT_HTTPHEADER, [ 'Content-Type: multipart/form-data; boundary=' . $post->boundary(), "Content-Length: " . strlen($data) ]);
     curl_setopt($this->curl, CURLOPT_POST, 1);
-    curl_setopt($this->curl, CURLOPT_POSTFIELDS, $post);
-    curl_setopt($this->curl, CURLOPT_URL, $this->url . "/webapi/entry.cgi?api=SYNO.Core.Certificate&method=import&version=1&SynoToken=$this->token");
+    curl_setopt($this->curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($this->curl, CURLOPT_URL, $this->url . "/webapi/entry.cgi?api=SYNO.Core.Certificate&method=import&version=1");
     $this->response = json_decode(curl_exec($this->curl));
-    return $this->syno_success();
+    return $this->success();
   }
 
 }
 
 class multipart_data
 {
-
-  private $postdata;
-  private $boundary;
+  private $_postdata;
+  private $_boundary;
 
   function __construct()
   {
-    $this->postdata = '';
-    $this->boundary = "---------------------" . md5(mt_rand() . microtime());
+    $this->_postdata = '';
+    $this->_boundary = '---------------------' . md5(mt_rand() . microtime());
   }
 
-  public function get_postdata()
+  public function postdata()
   {
-    return $this->postdata . "--" . $this->boundary . "--\r\n";
+    return $this->_postdata . "--$this->_boundary--\r\n";
   }
 
-  public function get_boundary()
+  public function boundary()
   {
-    return $this->boundary;
+    return $this->_boundary;
   }
 
-  public function addpart($name, $data, $filename = null)
+  public function addPart($name, $data, $filename = null)
   {
     if (isset($filename))
       $filename = "; filename=\"$filename\"";
     else
       $filename = '';
 
-    $this->postdata .= "--$this->boundary\r\n" .
-                       "Content-Disposition: form-data; name=\"$name\"$filename\r\n" .
-                       "Content-Type: application/x-x509-ca-cert\r\n\r\n" .
-                       "$data\r\n";
+    $this->_postdata .= "--$this->_boundary\r\n" .
+                        "Content-Disposition: form-data; name=\"$name\"$filename\r\n" .
+                        "Content-Type: application/x-x509-ca-cert\r\n\r\n" .
+                        "$data\r\n";
   }
 
-  public function addfile($name, $filename)
+  public function addFile($name, $filename)
   {
     $data = file_get_contents($filename);
-    $this->addpart($name, $data, $filename);
+    $this->addPart($name, $data, $filename);
   }
 
 }
